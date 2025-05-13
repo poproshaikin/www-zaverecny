ab0a2b53ce1462f54a28e9dc30b502aae20d7ef2
poproshaikin <stasstrong06@gmail.com>
2025-05-13 21:55:27 +0200

    implement GET endpoint to retrieve users from the database; remove unused POST endpoint

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,22 +1,9 @@
 import {NextRequest, NextResponse} from "next/server";
 import prisma from "@/db/prisma";
+import {User} from "@/db/types/user";
 
 export async function GET() {
     const users = await prisma.user.findMany();
 
     return NextResponse.json(users);
-}
-
-export async function POST(request: NextRequest) {
-    const body = await request.json();
-    const { name, email } = body;
-
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-        },
-    });
-
-    return NextResponse.json(user);
 }
